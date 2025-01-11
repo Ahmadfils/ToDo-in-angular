939330455e7ea5c9f9c87415cb2fc910d773803f
Ahmadfils <106875358+Ahmadfils@users.noreply.github.com>
2025-01-11 15:21:02 +0000

    initial

---
+++ ToDOApp/src/app/components/todos/todos.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todos',
+  imports: [],
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.css'
+})
+export class TodosComponent implements OnInit{
+
+  constructor(){}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
